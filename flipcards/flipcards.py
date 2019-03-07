f2bb2fb36604ad555bb12abf9ad949e16851f7f7
Didier Renard <didier.renard@comunidad.ub.edu.ar>
2019-03-07 15:38:10 -0300

    - cargado de jugadores desde carpeta players

--- flipcards/flipcards.py
+++ flipcards/flipcards.py
@@ -2,11 +2,31 @@ from flipcards.player import Flipbot
 from flipcards.tournament import Tournament
 from common.connie import *
 import random
+import importlib
 import time
+import os
+
+def load_players(colors):
+    current_color = 0
+    players = []
+    created = []
+    for obj in os.listdir(os.path.join(os.path.dirname(sys.argv[0]), 'players')):
+        if obj.endswith('.py'):
+            players.append(obj.rstrip('.py'))
+    players.remove('__init__')
+    for player_name in players:
+        player = importlib.import_module('players.' + player_name)
+        created.append(getattr(player, 'create')(colors[current_color]))
+        current_color = current_color + 1
+        if current_color >= len(colors):
+            current_color = 0
+    return created
 
 if __name__ == '__main__':
     start = time.time()
+    colors = [ UIColor.light_blue(), UIColor.light_red(), UIColor.light_green(), UIColor.light_yellow(), UIColor.light_magenta() ]
     t = Tournament(0.1)
-    t.add_player(Flipbot('Didier', UIColor.light_blue()))
+    for player in load_players(colors):
+        t.add_player(player)
     t.play()
     print('\nRun time: ' + str(time.time() - start))
