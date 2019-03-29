1deb2703a06bc3ad9658e90b1e9c7bfb7f7320ed
Didier Renard <didier.renard@hotmail.com>
2019-03-29 18:49:21 -0300

    - actualizadas notas

--- python/students/students2019.py
+++ python/students/students2019.py
@@ -134,17 +134,17 @@ class Students2019(Students):
         self.gabriel().add_exercise('3m2',   1.8)
 
         # class 4                       c   a   p  mp  ms  op  os   q
-        #self.alex().grade_class(       4,  1,  0,  5,  2,  0,  0,  0)
-        #self.franco().grade_class(     4,  0,  0,  5,  1,  0,  0,  0)
-        #self.belen().grade_class(      4,  0,  0,  5,  4,  0,  0,  0)
-        #self.federico().grade_class(   4,  0,  0,  5,  2,  0,  0,  0)
-        #self.andres().grade_class(     4,  0,  0,  5,  3,  0,  0,  0)
-        #self.francisco().grade_class(  4,  0,  0,  5,  4,  0,  0,  0)
-        #self.diego().grade_class(      4,  0,  0, 10,  5,  0,  0,  0)
-        #self.damian().grade_class(     4,  0,  0,  5,  3,  0,  0,  0)
-        #self.rocio().grade_class(      4,  0,  0,  5,  3,  0,  0,  0)
-        #self.joaquin().grade_class(    4,  0,  0,  0,  0,  0,  0,  0)
-        #self.gabriel().grade_class(    4,  0,  0,  5,  2,  0,  0,  0)
+        self.alex().grade_class(       4,  1,  0,  5,  2,  0,  0,  0)
+        self.franco().grade_class(     4,  0,  0,  5,  1,  0,  0,  0)
+        self.belen().grade_class(      4, 10,  0,  5,  4,  0,  0,  0)
+        self.federico().grade_class(   4, 10,  0,  5,  2,  0,  0,  0)
+        self.andres().grade_class(     4, 10,  0,  5,  3,  0,  0,  0)
+        self.francisco().grade_class(  4, 10,  0,  5,  4,  0,  0,  0)
+        self.diego().grade_class(      4,  0,  0, 10,  5,  0,  0,  0)
+        self.damian().grade_class(     4, 10,  0,  5,  3,  0,  0,  0)
+        self.rocio().grade_class(      4,  0,  0,  5,  3,  0,  0,  0)
+        self.joaquin().grade_class(    4, 10,  0,  0,  0,  0,  0,  0)
+        self.gabriel().grade_class(    4,  0,  0,  5,  2,  0,  0,  0)
 
         # class 5                       c   a   p  mp  ms  op  os   q
         #self.alex().grade_class(       5,  0,  0,  0,  0,  0,  0,  0)
