997aac8d9be882530918415e4aab65cde7f5df20
Didier Renard <didier.renard@hotmail.com>
2019-04-13 12:07:34 -0300

    - actualizadas las notas

--- python/students/students2019.py
+++ python/students/students2019.py
@@ -160,17 +160,17 @@ class Students2019(Students):
         self.gabriel().grade_class(    5, 10, 10,  1,  0,  0,  0,  9)
 
         # class 6                       c   a   p  mp  ms  op  os   q
-        #self.alex().grade_class(       6,  0,  0,  0,  0,  0,  0,  0)
-        #self.franco().grade_class(     6,  0,  0,  0,  0,  0,  0,  0)
-        #self.belen().grade_class(      6,  0,  0,  0,  0,  0,  0,  0)
-        #self.federico().grade_class(   6,  0,  0,  0,  0,  0,  0,  0)
-        #self.andres().grade_class(     6,  0,  0,  0,  0,  0,  0,  0)
-        #self.francisco().grade_class(  6,  0,  0,  0,  0,  0,  0,  0)
-        #self.diego().grade_class(      6,  0,  0,  0,  0,  0,  0,  0)
-        #self.damian().grade_class(     6,  0,  0,  0,  0,  0,  0,  0)
-        #self.rocio().grade_class(      6,  0,  0,  0,  0,  0,  0,  0)
-        #self.joaquin().grade_class(    6,  0,  0,  0,  0,  0,  0,  0)
-        #self.gabriel().grade_class(    6,  0,  0,  0,  0,  0,  0,  0)
+        self.alex().grade_class(       6, 10, 10,  1,  0,  0,  0,  0)
+        self.franco().grade_class(     6,  1,  0,  1,  0,  0,  0,  0)
+        self.belen().grade_class(      6,  1,  0,  7,  7,  0,  0,  0)
+        self.federico().grade_class(   6, 10, 10, 10, 10,  0,  0,  0)
+        self.andres().grade_class(     6, 10, 10, 10,  6,  0,  0,  0)
+        self.francisco().grade_class(  6, 10, 10,  1,  0,  0,  0,  0)
+        self.diego().grade_class(      6, 10, 10, 10,  8,  0,  0,  0)
+        self.damian().grade_class(     6,  1,  0,  1,  0,  0,  0,  0)
+        self.rocio().grade_class(      6, 10, 10,  1,  0,  0,  0,  0)
+        self.joaquin().grade_class(    6, 10, 10,  1,  0,  0,  0,  0)
+        self.gabriel().grade_class(    6,  1,  0,  1,  0,  0,  0,  0)
 
         # class 7                       c   a   p  mp  ms  op  os   q
         #self.alex().grade_class(       7,  0,  0,  0,  0,  0,  0,  0)
