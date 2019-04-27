b3f60f5ab4c8c049c2301f7c2b88eda7078c8a3d
Didier Renard <didier.renard@hotmail.com>
2019-04-26 21:38:59 -0300

    + agregados metodos faltantes en CProductInstance

--- java/src/ar/edu/ub/testing/Discount/CProductInstance.java
+++ java/src/ar/edu/ub/testing/Discount/CProductInstance.java
@@ -8,6 +8,18 @@ public class CProductInstance
         this.m_price = price;
     }
 
+    public CProduct product()
+    {
+        // TODO: implement
+        return null;
+    }
+
+    public float price()
+    {
+        // TODO: implement
+        return 0.0f;
+    }
+
     private CProduct m_product;
     private float m_price;
 }
